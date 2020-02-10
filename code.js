b66ed3512bbcb100ce338e8ac3a4be4cc32faa0f
Brian Purnell <purnellbp@gmail.com>
2020-02-10 14:28:40 -0600

    reaftored sayHello function

--- code.js
+++ code.js
@@ -4,13 +4,7 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!"
-    } else if (input === "Jane"){
-        return "Hello, Jane!"
-    } else {
-        return "Hello, Pat!"
-    }
+ return "Hello, " + input + "!";
 }
 
 
